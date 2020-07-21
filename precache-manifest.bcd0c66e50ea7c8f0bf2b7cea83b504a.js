a2cccd4d1794390287cec99366ab8d484bfa66d9
pavan-kalyan-12 <s.pavankalyanayyappa@gmail.com>
2020-07-21 22:23:52 +0530

    Updates

---
+++ precache-manifest.bcd0c66e50ea7c8f0bf2b7cea83b504a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d849cfbd0b53d2b80bf7b6dd2344cb2",
+    "url": "/expenses-tracker/index.html"
+  },
+  {
+    "revision": "e2d07038bc7ec0f4e9ec",
+    "url": "/expenses-tracker/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "b33da4ab08a9582fead0",
+    "url": "/expenses-tracker/static/js/2.feffba70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expenses-tracker/static/js/2.feffba70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d07038bc7ec0f4e9ec",
+    "url": "/expenses-tracker/static/js/main.02dee71f.chunk.js"
+  },
+  {
+    "revision": "5575f27cd80e856491ae",
+    "url": "/expenses-tracker/static/js/runtime-main.a9505576.js"
+  }
+]);
